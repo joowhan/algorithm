55cb56f31b99ebad1a3d335b28bb30f16e59a938
joowhan <70326417+joowhan@users.noreply.github.com>
2025-12-28 18:01:30 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.38 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(n):
+    mod = 1234567
+    dp = [0]*(n+1)
+    dp[1]=1
+    if n>=2:
+        dp[2] = 2
+    for i in range(3, n+1):
+        dp[i]=(dp[i-1]+dp[i-2])%mod
+    return dp[n]
